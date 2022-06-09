daf99079aae8cd2bf67fef79e4adc61ef3e6853c
Alex <andres.a.campos@navy.mil>
2022-06-09 00:15:40 -0700

    renaming the jsx files to js,  filling in content for the componetns and adding them to the app comonent

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Header from './Header'
+import Footer from './Footer'
+import Note from './Note'
+
+export default function App(){
+    return <div>
+        <Header/>
+        <Footer/>
+        <Note/>
+    </div>
+}
--- src/Footer.jsx
+++
@@ -1,3 +0,0 @@
-export default funtion Footer(){
-  return (<footer><)
-}
