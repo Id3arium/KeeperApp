b6896d0e235bda3592c4d7afe61de57051e0b3e8
Alex <andres.al.campos@gmail.com>
2022-06-21 22:08:54 -0700

    adding area to create new notes

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,13 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 export default function App() {
   return (
     <div>
       <Header />
+      <CreateArea />
       {notes.map((props) => (
         <Note {...props} />
       ))}
