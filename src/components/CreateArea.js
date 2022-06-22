af692d2dcc53f7b1042ee93693f1ad637f72cea1
Alex <andres.al.campos@gmail.com>
2022-06-21 22:34:53 -0700

    storing the input from the create area in a note obj

--- src/components/CreateArea.js
+++ src/components/CreateArea.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CreateArea() {
+  const [note, setNote] = useState({ title: "", content: "" });
+  function handleChange(e) {
+    console.log(e.target.value);
+    const { name, value } = e.target;
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+        <input
+          name="title"
+          placeholder="Title"
+          value={note.title}
+          onChange={handleChange}
+        />
+        <textarea
+          name="content"
+          placeholder="Take a note..."
+          rows="4"
+          value={note.content}
+          onChange={handleChange}
+        />
         <button>Add</button>
       </form>
     </div>
