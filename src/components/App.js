12ddcc2e61f5b682be080f137bd4759fda994d1a
Alex <andres.al.campos@gmail.com>
2022-06-21 22:45:59 -0700

    making app keep track of notes and passing in code to add note to create area

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import defaultNotes from "../notes";
 import CreateArea from "./CreateArea";
-
 export default function App() {
+  const [notes, setNotes] = useState(defaultNotes);
+  function addNote(note) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, note];
+    });
+  }
   return (
     <div>
       <Header />
-      <CreateArea />
+      <CreateArea handleAddNote={addNote} />
       {notes.map((props) => (
         <Note {...props} />
       ))}
