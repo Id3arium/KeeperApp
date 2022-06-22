6f164d503211ee8e2cded13438d73dd81812b67f
Alex <andres.al.campos@gmail.com>
2022-06-21 22:13:30 -0700

    adding close button to cards

--- src/components/Note.js
+++ src/components/Note.js
@@ -3,6 +3,7 @@ import React from "react";
 export default function Note(props) {
   return (
     <div className="note">
+      <button>X</button>
       <h1> {props.title} </h1>
       <p> {props.content} </p>
     </div>
