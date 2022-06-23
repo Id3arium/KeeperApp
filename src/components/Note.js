3214a7569bd5daa81986a4c166967bb2f2abeb34
Alex <andres.al.campos@gmail.com>
2022-06-23 00:15:11 -0700

    changing delete button to have an actual delete icon instead of the letter x

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,12 +1,14 @@
 import React from "react";
-
+import DeleteIcon from "@material-ui/icons/Delete";
 export default function Note(props) {
   const deleteNote = () => {
     props.onDelete(props.id);
   };
   return (
     <div className="note">
-      <button onClick={deleteNote}>X</button>
+      <button onClick={deleteNote}>
+        <DeleteIcon />
+      </button>
       <h1>
         {" "}
         {"(" + (props.id + 1) + ") "}
